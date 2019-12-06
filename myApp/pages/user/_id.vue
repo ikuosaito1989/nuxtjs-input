46e6987bf9c096b88dcc512da47c7cf1fef5fd3f
ikuo saito <ikuosaito1989@gmail.com>
2019-12-06 23:11:52 +0900

    url paramを表示

--- myApp/pages/user/_id.vue
+++ myApp/pages/user/_id.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <div>this user Pages</div>
+    <span>Message: {{ id }}</span>
     <div>pram id</div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data: () => ({
+    id: 0
+  }),
+  created() {
+    this.id = this.$route.params.id
+  }
+}
+</script>
